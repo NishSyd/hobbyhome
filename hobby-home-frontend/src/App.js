78fec2140f0a67086305a7b673f197402ccfd1c7
Nisha <nishakamanulla@gmail.com>
2022-12-28 19:04:38 +1100

    Initial change.

---
+++ hobby-home-frontend/src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="row">
+          <h1 className="mt-3"> Hobby home </h1>
+        </div>
+        <div>
+          <a href='#!'><span className="badge bg-success">Login</span></a>
+        </div>
+          <hr className="mb-3"></hr>
+      </div>  
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action"> Home </a>
+              <a href="#!" className="list-group-item list-group-item-action"> Hobby</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+        
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ hobby-home-frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+
+import hobbymarket from './../images/hobby-market.png';
+
+const Home = () => {
+    return (
+        <>
+        <div className="text-center">
+            <h2> Find a hobby to take home </h2>
+            <hr/>
+            <img src={hobbymarket} alt="Hobby Market"></img>
+        
+        </div>
+        
+        </>
+    )
+}
