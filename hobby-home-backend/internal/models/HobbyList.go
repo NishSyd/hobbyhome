92f962a837efde5085e1f834ef338764e2cdb439
Nisha <nishakamanulla@gmail.com>
2023-01-07 22:01:03 +1100

    Removed ID from hobby model.

--- hobby-home-backend/internal/models/HobbyList.go
+++ hobby-home-backend/internal/models/HobbyList.go
@@ -1,13 +1,9 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
 type HobbyItem struct {
-    Id          primitive.ObjectID       `json:"_id,omitempty" bson:"_id,omitempty`
-	Name        string    `json:"name"` 
-    Category    string    `json:"category"`
-	Cost        int       `json:"cost"`
-	Description string    `json:"description"`
-	Image       string    `json:"image"`
+	Name        string `json:"name"`
+	Category    string `json:"category"`
+	Cost        int    `json:"cost"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
 }
